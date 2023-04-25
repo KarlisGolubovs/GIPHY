d648eeaa4d16ff7f3eb23bdda136509f1edd6750
KarlisGolubovs <karlisgolubovs@gmail.com>
2023-04-25 03:33:28 +0300

    Commit

---
+++ app/getRequest.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+class getApiClient
+{
+    private Client $client;
+
+    public function __construct()
+    {
+        $this->client = new Client;
+    }
+
+    public function fetchData(string $apiKey, string $searchTerm) : array
+    {
+        $url = "https://api.giphy.com/v1/gifs/search?api_key=$apiKey&q=$searchTerm&limit=25&offset=0&rating=g&lang=en";
+        try {
+            $res = $this->client->request('GET', $url, [
+                'headers' => [
+                    'Accept' => 'application/json'
+                ]
+            ]);
+            return json_decode($res->getBody()->getContents(), true);
+        } catch (GuzzleException $e) {
+            return [];
+        }
+    }
+}
