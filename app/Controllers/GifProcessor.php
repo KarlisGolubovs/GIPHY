bf703f78f6e0f6206e72e7636b9cf03cf2d0d2f3
KarlisGolubovs <karlisgolubovs@gmail.com>
2023-05-02 21:55:08 +0300

    Commit

--- app/Controllers/GifProcessor.php
+++ app/Controllers/GifProcessor.php
@@ -3,22 +3,35 @@
 namespace Giphy\Controllers;
 
 use Giphy\Models\AppClient;
+use Giphy\Models\GIF;
+use GuzzleHttp\Exception\GuzzleException;
 
 class GifProcessor
 {
     private AppClient $client;
+    private AppClient $appClient;
 
-    public function __construct()
+    public function __construct(AppClient $appClient)
     {
         $this->client = new AppClient();
+        $this->appClient = $appClient;
     }
 
-    public function searchGif(): array
+    public function searchGif(string $query, int $limit = 10): array
     {
-        $search = $_POST['search'] ?? '';
-        return $this->client->search($search);
+        $response = $this->appClient->search($query, $limit);
+        $data = $response['data'] ?? [];
+        $gifs = [];
+        foreach ($data as $gif) {
+            $gifs[] = new GIF($gif['images']['original']['url']);
+        }
+        return $gifs;
     }
 
+
+    /**
+     * @throws GuzzleException
+     */
     public function getTrendingGifs(): array
     {
         return $this->client->trending();
